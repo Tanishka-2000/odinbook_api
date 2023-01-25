b3303e18d51d9d8e7160c79c320b711f93e8ffe9
Shashank Talwar <shashanktalwar81@gmail.com>
2023-01-25 11:49:21 +0530

    add route for user's and users's friends' recent posts and another route for user's friends

--- api/protected.js
+++ api/protected.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Post = require('../models/post');
 
 router.get('/', (req, res) => {
-  return res.json(req.user);
+  let posts = [];
+  User.findOne({_id: req.user._id}, 'posts friends')
+  .populate({
+    path: 'posts',
+    options: {
+      sort : {postedAt: -1}
+    },
+    limit: 3,
+    populate : {
+      path: 'author',
+      select: 'name image'
+    }
+  })
+  .populate({
+    path:'friends',
+    select: 'posts',
+    populate: {
+      path: 'posts',
+      options: {
+        sort : {postedAt: -1},
+      },
+      limit: 3,
+      populate: {
+        path: 'author',
+        select: 'name image'
+      }
+    },
+  })
+  .exec((err, user) => {
+    posts = [...user.posts];
+    user.friends.forEach(friend => {
+      posts = [...posts, ...friend.posts];
+    });
+    res.json(posts);
+    // res.json(user)
+  })
 });
+;
 
 router.get('/users', (req, res) => {
   User.findOne({_id: req.user._id}, 'friends', (err, user) => {
@@ -13,5 +50,13 @@ router.get('/users', (req, res) => {
     })
   })
 });
+
+router.get('/friends', (req, res) => {
+  User.findOne({_id: req.user._id}, 'friends')
+  .populate('friends', '_id name image')
+  .exec((err, user) => {
+    res.json(user.friends);
+  })
+})
 
 module.exports = router;
