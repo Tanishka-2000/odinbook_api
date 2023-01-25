41fbcf3cbb7a035b4ed706893b9c9dd21d39fe28
Shashank Talwar <shashanktalwar81@gmail.com>
2023-01-25 12:31:49 +0530

    add new post to database and post id to user's posts array

--- api/protected.js
+++ api/protected.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const User = require('../models/user');
 const Post = require('../models/post');
 
+// index route
 router.get('/', (req, res) => {
   let posts = [];
   User.findOne({_id: req.user._id}, 'posts friends')
@@ -41,7 +43,6 @@ router.get('/', (req, res) => {
     // res.json(user)
   })
 });
-;
 
 router.get('/users', (req, res) => {
   User.findOne({_id: req.user._id}, 'friends', (err, user) => {
@@ -57,6 +58,30 @@ router.get('/friends', (req, res) => {
   .exec((err, user) => {
     res.json(user.friends);
   })
-})
+});
+
+// posts route
+
+router.post('/posts', (req, res) => {
+  Post.create({
+    author: req.user._id,
+    message: req.body.message,
+    imageUrl: req.body.imageUrl,
+    tags: req.body.tags.split(' '),
+    postedAt: Date.now(),
+    likes: 0
+  }, (err, post) => {
+    User.updateOne({_id: req.user._id}, {$push: {posts : post._id}}, (err) => {
+      res.status(200).send();
+    });
+  });
+});
+
+// router.post('/posts/:postId/like', async (req, res) => {
+//   // check if user has already liked this post
+//   const user = await User.findById(req.user._id, 'friends').lean();
+//   console.log(typeof user.friends[0]);
+//   res.json(true);
+// });
 
 module.exports = router;
